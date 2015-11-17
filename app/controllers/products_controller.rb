26c3708333093761c41eb24a2c12d0b78e28eae7
jhsiaomei <jw1314@gmail.com>
2015-11-16 19:32:00 -0600

    CRUD set up in REST fashion

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,44 @@
 class ProductsController < ApplicationController
-  def home
-    @title = "Home"
-  end
-
-  def products
+  def index
     @title = "Products"
+    @products = Product.all
   end
 
-  def product
+  def new
+  end
+
+  def create
+    product = Product.create(
+        title: params[:title],
+        description: params[:description],
+        price: params[:price],
+        quantity: params[:quantity]
+      )
+    redirect_to '/products'
+  end
+
+  def show
     @title = "Product"
+    @product = Product.find_by(id: params[:id])
   end
 
-  def confirmation
-    @title = "Confirmation"
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    product = Product.find_by(id: params[:id])
+    product.update(
+        title: params[:title],
+        description: params[:description],
+        price: params[:price],
+        quantity: params[:quantity]
+      )
+    redirect_to "/products"
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get '/' => 'products#home'
-  get 'products' => 'products#products'
-  get 'product' => 'products#product'
-  get 'confirmation' => 'products#confirmation'
-  # The priority is based upon order of creation: first created -> highest priority.
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  post '/products' => 'products#create'
+  get '/products/:id' => 'products#show'
+  get '/products/:id/edit' => 'products#edit'
+  patch '/products/:id' => 'products#update'
+  delete '/products/:id' => 'products#destroy'
+
+   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
