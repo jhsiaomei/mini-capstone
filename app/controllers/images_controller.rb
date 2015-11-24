12e7b5d4ef9944bc43c14af5197449dc40acdab1
jhsiaomei <jw1314@gmail.com>
2015-11-23 20:14:43 -0600

    updated create in controller so images now are created in db

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,5 +3,11 @@ class ImagesController < ApplicationController
   end
 
   def create
+    Image.create(
+      name: params[:name],
+      link: params[:link],
+      product_id: params[:product_id]
+    )
+    flash[:success] = "You successfully uploaded a photo!"
   end
 end
