227c7d6b6b430ad4208fc89d8a73cf56ddbb600a
jhsiaomei <jw1314@gmail.com>
2015-11-25 18:51:29 -0600

    subtotal, tax, and total saved to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,13 @@
 class OrdersController < ApplicationController
   def create
+    product = Product.find_by(id: params[:product_id])
     order = Order.create(
       product_id: params[:product_id],
       quantity: params[:quantity],
-      user_id: current_user.id
+      user_id: current_user.id,
+      subtotal: product.price,
+      tax: product.tax,
+      total: product.total,
       )
     redirect_to "/orders/#{order.id}"
   end
