fcc172733a45b3f0feceebb0bd90847e67e15319
jhsiaomei <jw1314@gmail.com>
2015-11-25 18:35:52 -0600

    added orders controller, created form on product show page to purchase product, order saves with user id and quantity

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,12 @@
+class OrdersController < ApplicationController
+  def create
+    order = Order.create(
+      quantity: params[:quantity],
+      user_id: current_user.id
+      )
+    redirect_to "/orders/#{order.id}"
+  end
+
+  def show
+  end
+end
