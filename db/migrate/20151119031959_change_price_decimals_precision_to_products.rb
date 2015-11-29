ea045aac5560b2f55a9fabcc7b5c8c78a65524e5
jhsiaomei <jw1314@gmail.com>
2015-11-29 10:24:37 -0600

    updated migrations again (parentheses) for decimals to numeric

--- db/migrate/20151119031959_change_price_decimals_precision_to_products.rb
+++ db/migrate/20151119031959_change_price_decimals_precision_to_products.rb
@@ -1,5 +1,5 @@
 class ChangePriceDecimalsPrecisionToProducts < ActiveRecord::Migration
   def change
-        change_column :products, :price, 'numeric USING CAST(price AS numeric', precision: 8, scale: 2
+        change_column :products, :price, 'numeric USING CAST(price AS numeric)', precision: 8, scale: 2
   end
 end
